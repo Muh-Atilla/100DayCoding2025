a8a922acc5e6a6155b72bdf54cde9e3f13a4d06f
Muh-Atilla <tla419328@gmail.com>
2025-09-09 02:31:27 -0700

    Add files via upload

---
+++ Day2.java
@@ -0,0 +1,18 @@
+public class Day2 {
+    public static void main(String[] args) {
+        //contoh penggunaan println()
+        System.out.println("muh");
+        System.out.println("atilla");
+        //contoh penggunaan print()
+        System.out.print("muh");
+        System.out.print("atilla");
+        //penggunaan printf
+        String nama = "tilla";
+        int umur = 18;
+
+        System.out.printf("nama: %s\n" ,nama);
+        System.out.printf("umur: %dtahun\n" ,umur);
+
+
+    }
+}
