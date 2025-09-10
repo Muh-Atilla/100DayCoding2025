ab421dcb08b157dfb73997a245d506202c25e4dd
muh atilla <tla419328@gmail.com>
2025-09-10 02:36:57 -0700

    Add files via upload

---
+++ Day3.java
@@ -0,0 +1,11 @@
+/**
+ * program: komentar dokumentasi
+ * Deskripsi: penggunaan komentar dokumentasi
+ */
+public class Day3{
+    public static void main (String[] args) {
+        System.out.println("tilla");
+        /* komentar multi-baris penjelasan panjang*/
+        // komentar satu baris penjelasan singkat
+    }
+}
