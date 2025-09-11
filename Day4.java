54867cdeda966fce5e7d215412a48397f308afc6
muh atilla <tla419328@gmail.com>
2025-09-11 08:05:12 -0700

    Add files via upload

---
+++ Day4.java
@@ -0,0 +1,18 @@
+public class Day4 {
+    public static void main(String[] args) {
+         // \n untuk baris baru
+        System.out.println("hay till \nthnk you") ;
+        // \t untuk tab 
+        System.out.println("nama: \till");
+        // \" untuk tanda kutip
+        System.out.println("\"halo\"");
+        // \' tanda kutip tunggal
+        System.out.println("\'nice till\'");
+        // \menanpilkan karakter backslash
+        System.out.println("\\hello");
+        // \r crriage return
+         System.out.println("hello \rworld");
+         // \b backspace
+         System.out.println("hello\b!");      
+    }    
+}
