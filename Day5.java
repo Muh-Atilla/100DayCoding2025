62d05ca9791aa667e4f9bc68adec2c3551246fe7
muh atilla <tla419328@gmail.com>
2025-09-12 01:26:41 -0700

    Add files via upload

---
+++ Day5.java
@@ -0,0 +1,21 @@
+public class Day5{
+    public static void main(String[] args){
+        // ===== TIPE DATA BYTE =====
+        byte umur = 18; 
+        System.out.println("umur (byte); "+umur);
+        // ===== TIPE DATA INT =====
+        int jumlahpenduduk = 1800000000;
+        System.out.println("jumlah penduduk indonesia (int): "+jumlahpenduduk);
+        // ===== TIPE DATA LONG =====
+        long jarakbangunan = 12345678910L;
+        System.out.println("jarak bangunan terdkat ke bumi (long): " + jarakbangunan+ " km");
+        // ===== CONTOH PERHITUNGAN =====
+        int a = 100;
+        int b = 200;
+        int hasil = a + b;
+        System.out.println("hasil penjumlahan (int): " + hasil);
+        // ====== TIPE DATA SHORT ======
+        short jumlahpendudukdikotamajene = 1234;
+        System.out.println("jumlah penduduk dikota majene (short): " + jumlahpendudukdikotamajene); 
+    }
+}
