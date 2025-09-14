9a0557e103fa018180edcb4da320e688bf1559d4
muh atilla <tla419328@gmail.com>
2025-09-14 04:48:38 -0700

    Add files via upload

---
+++ Day7.java
@@ -0,0 +1,27 @@
+public class Day7 {
+    public static void main(String[] args){
+        //booleam
+        boolean hobiolahraga = true;
+        boolean sukakoding = false;
+        //nilai booleam
+        System.out.println("hobi olahraga" );
+        System.out.println("suka koding " );
+
+        //char
+        char hurufnamaakhir = 'A';
+        char inisial = 'A';
+        char simbol = '#';
+        //menampilkan
+        System.out.println("Huruf pertama: " + hurufnamaakhir);
+        System.out.println("huruf kedua: " + inisial);
+        System.out.println("simbol: " + simbol);
+        //operasi
+        char huruf3 = (char)(hurufnamaakhir + 1);
+        System.out.println("huruf setelah 'A' adalah: " + huruf3);
+
+
+
+    }
+}
+    
+       
