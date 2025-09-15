ef0634b4653cbce5c38286a94b9bcee4b157495d
muh atilla <tla419328@gmail.com>
2025-09-15 07:34:42 -0700

    Add files via upload

---
+++ Day8.java
@@ -0,0 +1,8 @@
+public class Day8 {
+    public static void main(String[] args) {
+        String nama = "Tilla!";
+        String sapa = " hay juga!";
+        System.out.println("Halo, " + nama);
+        System.out.println("iya, " + sapa);
+    }
+}
