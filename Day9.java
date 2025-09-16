a88278ee571bf8505a0a2cc1728c03c80ff1dd2e
muh atilla <tla419328@gmail.com>
2025-09-16 22:59:27 +0800

    Add files via upload

---
+++ Day9.java
@@ -0,0 +1,19 @@
+public class Day9 {
+    public static void main(String[] args) {
+        // Deklarasi dan inisialisasi variabel
+        int umur = 15;
+        double tinggi_badan = 155;
+        // Menampilkan nilai awal
+        System.out.println("nilai awal umur: " + umur);
+        System.out.println("tinggi badan awal: " + tinggi_badan);
+        System.out.println(); // baris kosong
+
+        // Update nilai variabel
+        umur = 18;
+        tinggi_badan = 165;
+        // Menampilkan nilai setelah update
+        System.out.println("setelah update");
+        System.out.println("umur: " + umur);
+        System.out.println("tinggi badan: " + tinggi_badan);
+    }
+}
