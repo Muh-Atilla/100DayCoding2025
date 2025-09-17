316be20dce00bffe7d4eca29e82fbf6405e1e4f2
muh atilla <tla419328@gmail.com>
2025-09-17 21:26:37 +0800

    Add files via upload

---
+++ Day10.java
@@ -0,0 +1,27 @@
+
+public class Day10{
+    public static void main(String[] args) {
+        final String Nama = "muh atilla";
+        final byte umur  = 18;
+        final short tahun_lahir = 2007;
+        final char golongan_darah= 'B';
+        final double tinggi_badan = 165.0;
+        final boolean masih_sekolah = false;
+        final boolean masih_kuliah = true;
+        final int angka_Faforit= 70;
+        final float berat_badan = 852.5f;
+        final long angka = 12345l;
+
+        System.out.println("nama: " + Nama);
+        System.out.println("umur: " + umur);
+        System.out.println("tahun_lahir: " + tahun_lahir);
+        System.out.println("golongan_darah: " + golongan_darah);
+        System.out.println("tinggi_badan: " + tinggi_badan);
+        System.out.println("masih_sekolah: " + masih_sekolah);
+        System.out.println("masih_kuliah: " + masih_kuliah);
+        System.out.println("angka_Faforit: " + angka_Faforit);
+        System.out.println("berat_badan: " + berat_badan);
+        System.out.println("angka: " + angka);
+    }
+
+}
